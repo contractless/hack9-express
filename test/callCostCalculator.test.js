0d86d0f881640003c8eb25081ec90e60cc493032
Nemanja Jeremic <jnemanja@hotmail.com>
2019-12-13 16:02:51 +0100

    added unit tests using mocha/chai

---
+++ test/callCostCalculator.test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert
+const { calculateCallCost, calculateDuration } = require('../functions/callCostCalculator');
+
+
+describe('Verify calculation functions', function () {
+
+    it('Calculates call cost', () => {
+        const callCost = calculateCallCost(60, 4);
+        assert.equal(callCost, 4);
+    });
+
+    it('Calculates call duration', () => {
+        const callDuration = calculateDuration(10, 30, 10);
+        assert.equal(callDuration, 40);
+    });
+});
