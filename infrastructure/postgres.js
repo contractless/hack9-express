79a4df10fb2fed3d70a9a4f6d8ecde910b83adce
Nebojsa Loznjakovic <nebojsa.loznjakovic@gmail.com>
2019-12-13 23:23:20 +0100

    Merge2

--- infrastructure/postgres.js
+++ infrastructure/postgres.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const connectionString = process.env.CONN_STR;
 
-<<<<<<< HEAD
 async function getItemByPrefixAndDateFromPostgres(prefixArg, date) {
     const client = await createClient();
 
@@ -14,24 +13,6 @@ async function getItemByPrefixAndDateFromPostgres(prefixArg, date) {
         where start_date <= '${date}'
         order by sim desc, pl desc
         limit 1`;
-=======
-async function getItemByPrefixAndDateFromPostgres(prefix, date) {
-    // const client = await createClient();
-    const client = new Client({
-        connectionString: connectionString
-    });
-    await client.connect();
-
-    const query = `
-    select similarity(prefix, '${prefix}') as sim, length(prefix) as pl, prefix, price, increment, initial, start_date
-    from public.calling_codes
-    where start_date <= '${date}'
-    order by sim desc, pl desc
-    limit 1
-    `;
-
-    let res;
->>>>>>> e0bd3b1afb877f2d3500f1a3cd1460db7cdb14ce
 
     try {
         const queryResult = await client.query(query);
@@ -42,15 +23,12 @@ async function getItemByPrefixAndDateFromPostgres(prefix, date) {
         }
         
         const { prefix, price, initial, increment, start_date } = queryResult.rows[0];
-<<<<<<< HEAD
 
         if(prefixArg.indexOf(prefix) == -1){
             client.end();
             return null;
         }
 
-=======
->>>>>>> e0bd3b1afb877f2d3500f1a3cd1460db7cdb14ce
         client.end();
 
         return new CallingCode(prefix, price, initial, increment, start_date);
