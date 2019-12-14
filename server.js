3ed19df296d46f11437708be415f5ffc456d93a2
Nebojsa Loznjakovic <nebojsa.loznjakovic@gmail.com>
2019-12-14 05:50:14 +0100

    test

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ fastify.get('/switch/price', async (req, res) => {
 
   const dbData = await getItemByPrefixAndDate(number, time);
 
-  if(!dbData) return res.status(404).send({message: 'Price for the number cannot be calculated!'})
+  // if(!dbData) return res.status(404).send({message: 'Price for the number cannot be calculated!'})
+  if(!dbData) return res.status(200).send({
+    prefix: "99999",
+    price: parseFloat("99999", 10),
+    from: "99999",
+    initial: parseInt("99999", 10),
+    increment: parseInt("99999", 10)
+  })
 
   const pricePerMinute = calculateCallCost(60, dbData.price);
 
