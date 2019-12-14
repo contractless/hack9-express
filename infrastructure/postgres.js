6a1666ccd6ce062dbf0ad821043e7d60fa862e07
Nebojsa Loznjakovic <nebojsa.loznjakovic@gmail.com>
2019-12-14 08:32:50 +0100

    oli

--- infrastructure/postgres.js
+++ infrastructure/postgres.js
@@ -22,7 +22,7 @@ async function getItemByPrefixAndDateFromPostgres(prefixArg, date) {
     const query = `
         select similarity(prefix, '${prefixArg}') as sim, length(prefix) as pl, prefix, price, increment, initial, start_date
         from public.calling_codes
-        where start_date <= '${date}' and ${prefixArg} like concat(prefix, '%')
+        where start_date <= '${date}' and '${prefixArg}' like concat(prefix, '%')
         order by sim desc, pl desc, start_date desc
         limit 1`;
 
